9930138252c8d6eded00185c292f588e3be9f972
Temi Michael <olasehindemichael285@gmail.com>
2024-07-02 00:28:30 +0100

    Working on Nav items and Social Buttons

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <main>
+            <aside>
+                <h3>About Nelius</h3>
+                
+            </aside>
+        </main>
+    )
+};
