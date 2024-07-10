f7f7868cd47c1436cbec843d6cb24c9c1d52746a
Temi Michael <olasehindemichael285@gmail.com>
2024-07-11 00:48:30 +0100

    Subscribe Component feature progress

--- src/components/sections/Subscribe.jsx
+++ src/components/sections/Subscribe.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Button from "../../assets/Button"
 
 export default function Subscribe() {
     const [email, setEmail] = useState('')
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -12,9 +24,13 @@ export default function Subscribe() {
             <p className="subscribeHead">
                 Ready To Get <span className="textOne">Funded</span>?
             </p>
-            <p className="subscribeText">Enter your email for early access. Our team wil get in touch for personal branding.</p>
-            <form action="" method="">
-                <div>
+            {isMobile ? (
+                <p className="subscribeText">Enter your email for early access. Our team will get in touch for personal branding.</p>
+            ) : (
+                <p className="subscribeText">Enter your email for early access. Our team <br /> will get in touch for personal branding.</p>
+            )}
+            <form action="" method="" className="form">
+                <div className="inputContainer">
                     <input type="email" className="emailInput" id="emailInput" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} onClick={handleSubmit} />
                     <Button button="Subscribe" />
                 </div>
